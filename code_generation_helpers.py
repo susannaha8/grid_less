d753ea7c512b4ca05ae0ac8235ec9fe0590a6a33
susannaha8 <66691218+susannaha8@users.noreply.github.com>
2023-06-03 00:28:48 -0400

    added functions

--- code_generation_helpers.py
+++ code_generation_helpers.py
@@ -62,10 +62,14 @@ def gen_add_func_doc(self, func_desc, notes = [], params = [], return_val = None
     self.gen_add_code_line(" */")
 
 def gen_add_serial_ops(self, use_thread_group = False):
-    """using the first 
+    """
+    In CUDA, multiple threads running allows for computation in parallel. To run a block of 
+    code serially, call gen_add_serial_ops(). This function makes sure that 
+    your operation is running only on the first thread.å
 
-    :param use_thread_group: _description_, defaults to False
-    :type use_thread_group: bool, optional
+    note: gen_add_serial_ops() opens a parenthesis. After all the code to be executed serially has been added,
+    call gen_add_end_control_flow() to close the parenthesis.
+
     """
     if use_thread_group:
         self.gen_add_code_line("if(tgrp.thread_rank() == 0){", True)
@@ -73,6 +77,20 @@ def gen_add_serial_ops(self, use_thread_group = False):
         self.gen_add_code_line("if(threadIdx.x == 0 && threadIdx.y == 0){", True)
 
 def gen_add_parallel_loop(self, var_name, max_val, use_thread_group = False, block_level = False):
+    """call gen_add_parallel_loop() when you are running an operation in parallel. After this call, add
+    in the operation. In the subsequent lines, you may need to use var_name to move through your shared memory.
+    For example: "int row = ind % 6;". 
+
+    :param var_name: name for an index variable to identify a thread, such as "index" or "ind"
+    :type var_name: string
+    :param max_val: number of operations to run in parallel (ex. str(36))
+    :type max_val: string
+
+    //add examples
+
+    note: gen_add_parallel_loop() opens a parenthesis. After all the code to be executed in parallel has been added,
+    call gen_add_end_control_flow() to close the parenthesis.
+    """
     if block_level:
         if use_thread_group:
             print("![ERROR]: BLOCK LEVEL THREAD GROUP LOOP NOT IMPLEMENTED YET")
@@ -95,10 +113,38 @@ def gen_static_array_ind_3d(self, ind, col, row, ind_stride = 36, col_stride = 6
     return ind_stride*ind + col_stride*col + row
 
 def gen_add_sync(self, use_thread_group = False):
+    """after executing a section of code in parallel, some threads might be finished sooner than others. To
+    make sure no threads are going past this point until all threads are finished with the parallel operation, 
+    gen_add_sync() inserts __syncthreads(), which syncs all the threads in the block. Call this at each step of
+    the algorithm.
+
+    """
     if use_thread_group:
         self.gen_add_code_line("tgrp.sync();")
     else:
         self.gen_add_code_line("__syncthreads();")
+
+#ADDED (IDSVA specific): //extend to print any shared memory
+def print_shared(self, var, ncol, size): #varname, number of columns in one matrix, number of matrices
+    """function I added for IDSVA to make error checking easier-- inserts a call to printMat to 
+    print all of the matrix values for given variable
+
+    :param var: name of variable to print
+    :type var: string
+    :param ncol: number of columns in one matrix of the variable
+    :type ncol: int
+    :param size: number of matrices of the variable
+    :type size: int
+    """
+    #s_a, 7, 15; s_b, 6, 28
+    content = "for (int i = 0; i < " + str(size) + "; i++) {" +\
+            "printf(\"" + var + "[%d]\\n\", i);" +\
+            "printMat<T,6," + str(ncol) + ">(&" + var + "[" + str(6 * ncol) + "*i], 6);}"
+    self.gen_add_sync()
+    self.gen_add_serial_ops() 
+    self.gen_add_code_line(content)
+    self.gen_add_end_control_flow()
+        
 
 def gen_var_in_list(self, var_name, option_list):
     if len(option_list) == 1:
